6c274db259ec8bd58f34b7d9046a351d79bd019e
Yousuf Zaman <redikod@gmail.com>
2018-03-10 23:00:25 +0600

    count div

--- src/com/codility/solutions/CountDiv.java
+++ src/com/codility/solutions/CountDiv.java
@@ -1,12 +1,50 @@
 package com.codility.solutions;
 
-// Credit Goes To dominik-sze
-// https://github.com/dominik-sze/Codility
 
 import java.lang.Math.*;
 
 class CountDiv {
-    public int solution(int A, int B, int K) {
+    
+	public static void main(String[] args) {
+		System.out.println("solution " + solution2(6, 11, 2));
+	}
+	
+	/**
+	 * Credit Goes To dominik-sze
+	 * https://github.com/dominik-sze/Codility
+	 */
+	public static int solution(int A, int B, int K) {
         return (int)Math.floor((double)(B-A)/(double)K) + (B%K < A%K || A%K==0 ? 1 : 0);
     }
+
+	/**
+	 * https://github.com/karimhamdanali/codility/blob/master/src/codility/CountDiv.java
+	 * @author karim
+	 * Time: O(1)
+	 * Space: O(1)
+	 */
+ 	public static int solution2(int A, int B, int K) {
+         // write your code in Java SE 8
+ 		return (B/K) - (A/K) + (A%K == 0 ? 1 : 0);
+     }
+
+	/**
+	 * https://github.com/hyogij/Algorithm/blob/master/codility/Lesson3.CountDiv/src/codility/CountDiv.java
+	 * Author : Hyogi Jung(hyogij@gmail.com)
+	 */
+ 	public static int solution3(int A, int B, int K) 
+ 	{
+		if(B < K) 
+		{
+			return 0;
+		}
+		int num1 = A / K;
+		
+		if(A % K != 0) 
+		{
+			num1++;
+		}
+		int num2 = B / K;
+		return num2 - num1 + 1;
+	}
 }
